09131ce0377502fe3a1649ae7f1f91b569309363
David Wallace Croft <david@CroftSoft.com>
2025-07-16 07:22:59 -0500

    ch01_p007_shared

---
+++ src/ch01_p007_shared/mod.rs
@@ -0,0 +1,46 @@
+#[allow(unused)]
+use ::std::{
+  any::Any,
+  cell::{Cell, RefCell, UnsafeCell},
+  collections::VecDeque,
+  marker::PhantomData,
+  mem::{ManuallyDrop, MaybeUninit},
+  ops::{Deref, DerefMut},
+  ptr::NonNull,
+  rc::Rc,
+  sync::{
+    atomic::{Ordering::*, *},
+    *,
+  },
+  thread::{self, JoinHandle, Scope, Thread, ThreadId},
+};
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test1() {
+    static X: [i32; 3] = [
+      1, 2, 3,
+    ];
+
+    let join_handle_0: JoinHandle<&[i32; 3]> = thread::spawn(|| dbg!(&X));
+
+    let join_handle_1: JoinHandle<&[i32; 3]> = thread::spawn(|| dbg!(&X));
+
+    let result_0: Result<&[i32; 3], Box<dyn Any + Send + 'static>> =
+      join_handle_0.join();
+
+    let result_1: Result<&[i32; 3], Box<dyn Any + Send + 'static>> =
+      join_handle_1.join();
+
+    let x_0: &[i32; 3] = result_0.unwrap();
+
+    let x_1: &[i32; 3] = result_1.unwrap();
+
+    assert_eq!(x_0, &X);
+
+    assert_eq!(x_1, &X);
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 mod ch01_p002_threads;
 mod ch01_p005_scoped;
+mod ch01_p007_shared;
